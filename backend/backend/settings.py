cf9a7602b93b46cf5c952655c5430c234e927f63
Samandar-Komilov <komilov.samandar07@gmail.com>
2024-05-07 11:45:30 +0500

    cors-headers setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     #external
     'graphene_django',
+    'corsheaders',
 ]
 
 GRAPHENE = {
@@ -50,6 +51,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",)
 
 TEMPLATES = [
     {
